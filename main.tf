bd697c17c1014cb0e295b684edf1e5a1e9e36638
Ubuntu <ubuntu@ip-172-31-33-72.eu-north-1.compute.internal>
2026-01-03 07:31:09 +0000

    Major Upgrade: Implemented Remote S3 Backend and DynamoDB State Locking

--- main.tf
+++ main.tf
@@ -1,23 +1,33 @@
 terraform {
+  required_version = ">= 1.2.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-  required_version = ">= 1.2.0"
+
+  # This is the "Remote State" magic part
+  backend "s3" {
+    bucket         = "zubair-terraform-state-514348993510"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-north-1"             # The region where your bucket lives
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "eu-north-1" # Stockholm (to match your S3 bucket)
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c7217cdde317cfec" # Updated Ubuntu 22.04 LTS for us-east-1
+  ami           = "ami-0fa91bc90632c73c9" # Ubuntu 22.04 LTS for eu-north-1
   instance_type = "t3.micro"
 
   tags = {
-    Name = "Zubair_Terraform_Demo"
+    Name = "Zubair_Terraform_Remote_Demo"
   }
 }
 
